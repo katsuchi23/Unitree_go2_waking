f2a70f52e21fca5ef23630c3af08463f13c17bd6
katsuchi23 <reynaldywidjaja3@gmail.com>
2025-08-16 15:02:05 +0800

    Initial commit

---
+++ source/unitree_go2_direct/unitree_go2_direct/tasks/direct/unitree_go2_direct/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Unitree-Go2-Direct-Direct-v0",
+    entry_point=f"{__name__}.unitree_go2_direct_env:UnitreeGo2DirectEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.unitree_go2_direct_env_cfg:UnitreeGo2DirectEnvCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
